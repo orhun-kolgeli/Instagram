b3b21c126549d1f25c542823f69f71f924d3dfbc
Orhun Kolgeli <74283397+orhun-kolgeli@users.noreply.github.com>
2022-06-14 16:15:36 -0700

    Add functionality to querying the Parse database

--- app/src/main/java/com/orhunkolgeli/parstagram/MainActivity.java
+++ app/src/main/java/com/orhunkolgeli/parstagram/MainActivity.java
@@ -5,19 +5,37 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
     Button btnLogout;
+    Button btnSubmit;
+    ImageView ivPostImg;
+    Button btnTakePic;
+    EditText etDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etDescription = findViewById(R.id.etDescription);
+        btnTakePic = findViewById(R.id.btnTakePic);
+        ivPostImg = findViewById(R.id.ivPostImg);
+        btnSubmit = findViewById(R.id.btnSubmit);
         btnLogout = findViewById(R.id.btnLogout);
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,7 +45,32 @@ public class MainActivity extends AppCompatActivity {
                 ParseUser.logOutInBackground();
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
+                // Prevent user from using back button to go back to Main Activity
+                finish();
             }
         });
+        // Get Instagram posts from the Parse database
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        // Include user information
+        query.include(Post.KEY_USER);
+        // To get Instagram posts from the database, call findInBackground
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) { // exception thrown
+                    Log.e(TAG, "Couldn't get posts", e);
+                    return;
+                }
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + " User: " + post.getUser().getUsername());
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/orhunkolgeli/parstagram/ParseApplication.java
+++ app/src/main/java/com/orhunkolgeli/parstagram/ParseApplication.java
@@ -3,11 +3,17 @@ package com.orhunkolgeli.parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register parse model "Post"
+        ParseObject.registerSubclass(Post.class);
+
+        // Initialize
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("mvMUADchHWi66NbHBC2IvI156NUFjJMpKUB8JLBE")
                 .clientKey("V5URBkRg9Y8LrZf17WnQgd8dau2UlNlbIqeu5yQA")
