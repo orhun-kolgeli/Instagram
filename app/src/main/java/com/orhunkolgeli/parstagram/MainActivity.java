88bc71ea52824b552662979e23f2c79ac1401022
Orhun Kolgeli <74283397+orhun-kolgeli@users.noreply.github.com>
2022-06-14 08:59:40 -0700

    Allow user to log out

--- app/src/main/java/com/orhunkolgeli/parstagram/MainActivity.java
+++ app/src/main/java/com/orhunkolgeli/parstagram/MainActivity.java
@@ -3,16 +3,31 @@ package com.orhunkolgeli.parstagram;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
-    Toolbar toolbar;
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = ParseUser.getCurrentUser().getUsername().toString();
+                Toast.makeText(MainActivity.this, user, Toast.LENGTH_SHORT).show();
+                ParseUser.logOutInBackground();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
