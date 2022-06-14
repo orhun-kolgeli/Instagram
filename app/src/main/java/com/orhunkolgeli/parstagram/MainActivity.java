5775e62ef5b4d362410999f73d5522fd0eec0d6c
Orhun Kolgeli <74283397+orhun-kolgeli@users.noreply.github.com>
2022-06-13 23:37:04 -0700

    Create toolbar, add Insta logo

--- app/src/main/java/com/orhunkolgeli/parstagram/MainActivity.java
+++ app/src/main/java/com/orhunkolgeli/parstagram/MainActivity.java
@@ -1,14 +1,18 @@
 package com.orhunkolgeli.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
