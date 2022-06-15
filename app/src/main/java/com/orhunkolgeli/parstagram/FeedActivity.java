71006b22e3fe56202fae4d928c0548f490531e41
Orhun Kolgeli <74283397+orhun-kolgeli@users.noreply.github.com>
2022-06-15 16:28:53 -0700

    Implement swipe-to-refresh-feed functionality

--- app/src/main/java/com/orhunkolgeli/parstagram/FeedActivity.java
+++ app/src/main/java/com/orhunkolgeli/parstagram/FeedActivity.java
@@ -3,6 +3,7 @@ package com.orhunkolgeli.parstagram;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +18,7 @@ import java.util.List;
 
 public class FeedActivity extends AppCompatActivity {
     public static final String TAG = "FeedActivity";
+    SwipeRefreshLayout swipeContainer;
     RecyclerView rvPosts;
     PostAdapter adapter;
     List<Post> allPosts;
@@ -37,6 +39,24 @@ public class FeedActivity extends AppCompatActivity {
         rvPosts.setLayoutManager(new LinearLayoutManager(this));
         // Query posts from Parse
         queryPosts();
+
+        // Set up swipe-to-refresh
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                adapter.clear();
+                queryPosts();
+                // Call setRefreshing(false) to signal refresh has finished
+                swipeContainer.setRefreshing(false);
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
     }
 
     private void queryPosts() {
