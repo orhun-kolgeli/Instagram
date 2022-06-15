5d10b3b3c7b63b2e46f653a6c4c244506464bb55
Orhun Kolgeli <74283397+orhun-kolgeli@users.noreply.github.com>
2022-06-14 17:49:04 -0700

    Allow user to take a photo and post to database

--- app/src/main/java/com/orhunkolgeli/parstagram/MainActivity.java
+++ app/src/main/java/com/orhunkolgeli/parstagram/MainActivity.java
@@ -1,10 +1,17 @@
 package com.orhunkolgeli.parstagram;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,13 +22,19 @@ import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+    public String photoFileName = "photo.jpg";
+    File photoFile;
     Button btnLogout;
     Button btnSubmit;
     ImageView ivPostImg;
@@ -37,6 +50,13 @@ public class MainActivity extends AppCompatActivity {
         ivPostImg = findViewById(R.id.ivPostImg);
         btnSubmit = findViewById(R.id.btnSubmit);
         btnLogout = findViewById(R.id.btnLogout);
+        btnTakePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchCamera();
+
+            }
+        });
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,12 +65,101 @@ public class MainActivity extends AppCompatActivity {
                 ParseUser.logOutInBackground();
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
-                // Prevent user from using back button to go back to Main Activity
+                // Prevent user from using back button to go back to Main Activity after logout
                 finish();
             }
         });
         // Get Instagram posts from the Parse database
-        queryPosts();
+        //queryPosts();
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String description = etDescription.getText().toString();
+                if (description.isEmpty()) {
+                    Toast.makeText(MainActivity.this,
+                            "Description cannot be empty!", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (photoFile == null || ivPostImg.getDrawable() == null) {
+                    Toast.makeText(MainActivity.this,
+                            "No image found!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                ParseUser user = ParseUser.getCurrentUser();
+                savePost(description, user, photoFile);
+            }
+        });
+    }
+
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // Load the taken image into a the ImageView ivPostImg
+                ivPostImg.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+        return file;
+    }
+
+    private void savePost(String description, ParseUser user, File photoFile) {
+        // Create a new Post object
+        Post post = new Post();
+        // Add the user and description to the post
+        post.setDescription(description);
+        post.setUser(user);
+        // Add the taken photo to the post
+        post.setImage(new ParseFile(photoFile));
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) { // exception thrown
+                    Log.e(TAG, "Error while saving", e);
+                    return;
+                }
+                Log.i(TAG, "Successfully saved post");
+                // Clear the description EditText
+                etDescription.setText("");
+                // Clear the image from user's view
+                ivPostImg.setImageResource(0);
+            }
+        });
+
     }
 
     private void queryPosts() {
