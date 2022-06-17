f7eae54fb31c032af16dd7da16ea421700ba9da1
Orhun Kolgeli <74283397+orhun-kolgeli@users.noreply.github.com>
2022-06-16 23:20:43 -0700

    Implement endless scrolling in FeedActivity

--- app/src/main/java/com/orhunkolgeli/parstagram/FeedActivity.java
+++ app/src/main/java/com/orhunkolgeli/parstagram/FeedActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -21,11 +22,14 @@ import java.util.List;
 
 public class FeedActivity extends AppCompatActivity {
     public static final String TAG = "FeedActivity";
+    public static final int QUERY_ITEM_COUNT = 5;
     SwipeRefreshLayout swipeContainer;
     RecyclerView rvPosts;
     PostAdapter adapter;
     List<Post> allPosts;
     Button btnLogout;
+    EndlessRecyclerViewScrollListener scrollListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +44,21 @@ public class FeedActivity extends AppCompatActivity {
         // Set the adapter on the recycler view
         rvPosts.setAdapter(adapter);
         // Set the layout manager on the recycler view
-        rvPosts.setLayoutManager(new LinearLayoutManager(this));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvPosts.setLayoutManager(linearLayoutManager);
+        // Set up scroll listener
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to the bottom of the list
+                loadNextDataFromApi(page);
+            }
+        };
+        // Adds the scroll listener to RecyclerView
+        rvPosts.addOnScrollListener(scrollListener);
         // Query posts from Parse
-        queryPosts();
+        queryPosts(0);
 
         btnLogout = findViewById(R.id.btnLogout);
         btnLogout.setOnClickListener(new View.OnClickListener() {
@@ -64,43 +80,52 @@ public class FeedActivity extends AppCompatActivity {
             @Override
             public void onRefresh() {
                 adapter.clear();
-                queryPosts();
+                queryPosts(0);
                 // Call setRefreshing(false) to signal refresh has finished
                 swipeContainer.setRefreshing(false);
+                // Reset endless scroll listener when performing a new search
+                scrollListener.resetState();
             }
         });
         // Configure the refreshing colors
-        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
-                android.R.color.holo_red_light);
+        swipeContainer.setColorSchemeResources(
+                R.color.insta_blue,
+                R.color.insta_purple_red,
+                R.color.insta_yellow,
+                R.color.insta_purple);
     }
 
-    private void queryPosts() {
+    private void loadNextDataFromApi(int page) {
+        queryPosts(page);
+    }
+
+    private void queryPosts(int page) {
         // specify what type of data we want to query - Post.class
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         // include data referred by user key
         query.include(Post.KEY_USER);
+        // Skip the posts that have already been shown
+        query.setSkip(page*QUERY_ITEM_COUNT);
         // limit query to latest 20 items
-        query.setLimit(20);
+        query.setLimit(QUERY_ITEM_COUNT); // FOR TESTING PURPOSES
         // order posts by creation date (newest first)
         query.addDescendingOrder("createdAt");
         // start an asynchronous call for posts
         query.findInBackground(new FindCallback<Post>() {
             @Override
             public void done(List<Post> posts, ParseException e) {
-                // check for errors
+                // Check for errors
                 if (e != null) {
                     Log.e(TAG, "Issue with getting posts", e);
                     return;
                 }
-                // for debugging purposes let's print every post description to logcat
+                // For debugging purposes print every post description to logcat
                 for (Post post : posts) {
                     Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
                 }
-                // save received posts to list and notify adapter of new data
+                // Save received posts to list and notify adapter of new data
                 allPosts.addAll(posts);
-                adapter.notifyDataSetChanged();
+                adapter.notifyItemRangeInserted(page*QUERY_ITEM_COUNT, QUERY_ITEM_COUNT);
             }
         });
     }
